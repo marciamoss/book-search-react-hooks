68ecc2a2e185c7d513c3cb4f67c9f1647a1860cd
Marcia Moss <marcia_moss@hotmail.com>
2022-10-06 20:25:29 -0400

    loading and error states updated

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -7,11 +7,17 @@ const BookSearch = () => {
   const [bookName, setBookName] = useState("");
   const [author, setAuthor] = useState("");
   const [debouncedBookName, setDebouncedBookName] = useState(bookName);
-  const [results, setResults] = useState([]);
+  const [booksList, setBooksList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [booksPerPage] = useState(5);
+  const [isLoading, setIsLoading] = useState(false);
+  const [noBooksFound, setNoBooksFound] = useState(false);
+  const [apiError, setApiError] = useState('');
 
   useEffect(() => {
+    setNoBooksFound(!bookName ? false : '');
+    setIsLoading(!bookName ? false : '');
+    setApiError('');
     let bookNAuthor=bookName ? (bookName).split(" ").join("+")+(author).split(" ").join("+") : '';
     const timerId = setTimeout(() => {
         setDebouncedBookName(bookNAuthor);
@@ -26,25 +32,33 @@ const BookSearch = () => {
     const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=:";
     const APIKEY = "&key=AIzaSyA5c5WsLSg30PL4WJkx92HtHn8JitM_DEo&startIndex=0&maxResults=40";
     const search = async () => {
-        const { data } = await axios.get(BASEURL + debouncedBookName + APIKEY).catch((error) => console.log("fetch books errored", error));
-        setResults(data.items);
+        const response = await axios.get(BASEURL + debouncedBookName + APIKEY).catch((error) => {
+            setNoBooksFound(false);
+            setIsLoading(false);
+            setApiError(`Api error: ${error?.message}`);
+        });
+        setBooksList(response?.data?.items);
+        setNoBooksFound(!response?.data?.items ? true : '');
+        setIsLoading(false);
     };
     if (debouncedBookName) {
+        setIsLoading(true);
+        setBooksList([]);
         search();
     }
   }, [debouncedBookName]);
 
   const indexOfLastBook = currentPage * booksPerPage;
   const indexOfFirstBook = indexOfLastBook - booksPerPage;
-  const currentBooks = results?.slice(indexOfFirstBook, indexOfLastBook);
+  const currentBooks = booksList?.slice(indexOfFirstBook, indexOfLastBook);
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
-  const renderedResults = (currentBooks?.length > 0 && bookName) ? currentBooks.map((result) => {
+  const renderedResults = (currentBooks?.length > 0 && bookName) ? currentBooks?.map((result) => {
     return (
         <div className="ui items container" key={result.id}>
             <div className="item">
                 <div className="ui small image">
-                    <img src={result.volumeInfo?.imageLinks?.smallThumbnail} alt=""/>
+                    <img src={result.volumeInfo?.imageLinks?.smallThumbnail} alt="NoImageAvailable"/>
                 </div>
                 <div className="content">
                     <div className="header">{result.volumeInfo.title} {result.volumeInfo.authors ? `by ${result.volumeInfo.authors}` : ''}</div>
@@ -67,7 +81,7 @@ const BookSearch = () => {
             </div>
         </div>
     );
-  }) : <div className="ui items container no-data-label"><p>No Results to Display</p></div>;
+  }) : '';
 
   return (
     <div>
@@ -93,7 +107,7 @@ const BookSearch = () => {
                 <div>Go to page
                     <Pagination
                         booksPerPage={booksPerPage}
-                        totalBooks={results?.length}
+                        totalBooks={booksList?.length}
                         paginate={paginate}
                         currentPage={currentPage}
                     />
@@ -101,6 +115,18 @@ const BookSearch = () => {
                 : ''
             }
         </div>
+        {isLoading ?
+            <div className="ui items container loading"><p>Loading.....</p></div>
+            : ''
+        }
+        {noBooksFound ?
+            <div className="ui items container no-data-label"><p>No Books Found</p></div>
+            : ''
+        }
+        {apiError ?
+            <div className="ui items container no-data-label"><p>{apiError}</p></div>
+            : ''
+        }
         <div className="ui celled list">{renderedResults}</div>
     </div>
   );
